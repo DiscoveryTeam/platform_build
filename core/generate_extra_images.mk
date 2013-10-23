77094ecc7f9a202b8be7bcd6ae9ce67466ee6fb8
Steve Kondik <shade@chemlab.org>
2013-10-22 22:23:27 -0700

    build: Add script to generate extra images
    Change-Id: I53081c7a2b4f18bc8c8a27b04d311a6b078b2022
    
    Use CM standard qcom naming
    
    Change-Id: I360b3dcf1fe82140a7993189e881f9d77b571bef
    
    generate_extra_images: Remove deprecated image generation
    
    * Remove msm7x27a/msm7x30 nand images
    * Remove msm8930 1G userdata
    
    Change-Id: I81b93d34893d504a53f2aadb9c3ac2089c06860c
    
    generate_extra_images: Rename targets and add 'sec' prefix
    
    Change-Id: Ie7debfb33349d16d9a457ff817072af2f3a10c0f

--- core/generate_extra_images.mk
+++ core/generate_extra_images.mk
@@ -9,7 +9,6 @@
 # variables again.
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
-INSTALLED_USERDATAIMAGE_TARGET := $(PRODUCT_OUT)/userdata.img
 
 #----------------------------------------------------------------------
 # Generate secure boot & recovery image
@@ -78,31 +77,28 @@ ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_PERSISTIMAGE_TARGET)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_PERSISTIMAGE_TARGET)
 
 #----------------------------------------------------------------------
-# Generate extra userdata images (for variants with multiple mmc sizes)
+# Generate device tree image (dt.img)
 #----------------------------------------------------------------------
-ifneq ($(BOARD_USERDATAEXTRAIMAGE_PARTITION_SIZE),)
-
-ifndef BOARD_USERDATAEXTRAIMAGE_PARTITION_NAME
-  BOARD_USERDATAEXTRAIMAGE_PARTITION_NAME := extra
+ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DT)),true)
+ifeq ($(strip $(BUILD_TINY_ANDROID)),true)
+include device/qcom/common/dtbtool/Android.mk
 endif
 
-BUILT_USERDATAEXTRAIMAGE_TARGET := $(PRODUCT_OUT)/userdata_$(BOARD_USERDATAEXTRAIMAGE_PARTITION_NAME).img
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbTool$(HOST_EXECUTABLE_SUFFIX)
 
-define build-userdataextraimage-target
-    $(call pretty,"Target EXTRA userdata fs image: $(INSTALLED_USERDATAEXTRAIMAGE_TARGET)")
-    @mkdir -p $(TARGET_OUT_DATA)
-    $(hide) $(MKEXTUSERIMG) -s $(TARGET_OUT_DATA) $@ ext4 data $(BOARD_USERDATAEXTRAIMAGE_PARTITION_SIZE)
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+
+define build-dtimage-target
+    $(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+    $(hide) $(DTBTOOL) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
     $(hide) chmod a+r $@
-    $(hide) $(call assert-max-image-size,$@,$(BOARD_USERDATAEXTRAIMAGE_PARTITION_SIZE),yaffs)
 endef
 
-INSTALLED_USERDATAEXTRAIMAGE_TARGET := $(BUILT_USERDATAEXTRAIMAGE_TARGET)
-$(INSTALLED_USERDATAEXTRAIMAGE_TARGET): $(INSTALLED_USERDATAIMAGE_TARGET)
-	$(build-userdataextraimage-target)
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
+	$(build-dtimage-target)
 
-ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_USERDATAEXTRAIMAGE_TARGET)
-ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_USERDATAEXTRAIMAGE_TARGET)
-
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTIMAGE_TARGET)
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_DTIMAGE_TARGET)
 endif
 
 .PHONY: aboot
