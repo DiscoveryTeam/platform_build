2a7080285c51aa7d6c8d8381f88684e05203907d
Salvatore Festa <salvatorefestatech@gmail.com>
2018-03-21 17:17:27 +0100

    build: Include the new sepolicy

--- core/config.mk
+++ core/config.mk
@@ -977,4 +977,4 @@ endif
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/discovery/sepolicy/sepolicy.mk)
+$(eval include vendor/discovery/sepolicy/common/sepolicy.mk)
