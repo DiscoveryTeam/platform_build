SilverShades02/build

build: Include common custom BoardConfig
Change-Id: I2fa56d400ca2adf371dc3edb37e47619c7dd0a8e

core/config.mk
@@ -225,6 +225,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
+-include vendor/aosp/config/BoardConfig.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:

DiscoveryTeam/platform_build

core/config.mk
@@ -225,6 +225,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
+-include vendor/discovery/config/BoardConfig.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers: